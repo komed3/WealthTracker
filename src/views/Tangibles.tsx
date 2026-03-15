3b3a53109b062f0dfd0b0d7b8b327f8a9fda6e91
komed3 <pk97@outlook.de>
2026-03-15 20:51:11 +0100

    minor bug fixes

--- src/views/Tangibles.tsx
+++ src/views/Tangibles.tsx
@@ -93,7 +93,7 @@ export default function Tangibles ( { data, t }: TangiblesProps ) {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
             {/* Grid of items */}
             <div className="lg:col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-4">
-                { items.map( ( item ) => ( <div key={item.id} className="p-6 rounded-2xl border border-brand-100 shadow-sm flex flex-col justify-between min-h-[160px]">
+                { items.map( ( item ) => ( <div key={item.id} className="bg-white p-6 rounded-2xl border border-brand-100 shadow-sm flex flex-col justify-between min-h-[160px]">
                     <div className="flex justify-between items-start">
                         <div className="p-3 bg-white rounded-xl shadow-sm">{item.icon}</div>
                     </div>
