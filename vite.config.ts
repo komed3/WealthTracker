57d231b037cdbba0c4450ec0d2b5688ea3cb1900
komed3 <pk97@outlook.de>
2026-03-15 15:44:08 +0100

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'node:path';
+
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig( ( { mode } ) => {
+    const env = loadEnv( mode, '.', '' );
+
+    return {
+        plugins: [ react(), tailwindcss() ],
+        define: { 'process.env.GEMINI_API_KEY': JSON.stringify( env.APIKEY ) },
+        resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+        server: { hmr: process.env.DISABLE_HMR !== 'true' }
+    };
+} );
