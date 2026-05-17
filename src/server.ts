0031c3702c840c5998d16d5b5be1551330e0d0ac
komed3 <pk97@outlook.de>
2026-05-17 12:17:07 +0200

    create vite server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import { createServer } from 'vite';
 
 async function run () : Promise< void > {
   const app = express();
   const port = process.env.PORT || 3000;
-  
+
+  const vite = await createServer( {
+    server: { middlewareMode: true }
+  } );
+
   app.use( express.json() );
   app.use( express.urlencoded( { extended: true } ) );
-  
+  app.use( vite.middlewares );
+
   app.listen( port, () => console.log( `Server startet on ::${ port }` ) );
 }
 
