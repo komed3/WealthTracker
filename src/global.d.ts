6006cc0c0a3bb701100d36c4a42d1159b2c6e880
komed3 <pk97@outlook.de>
2026-05-17 14:11:07 +0200

    declare module 'i18next'

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,10 @@
+import type { resources } from '@/src/lib/i18n';
+
 declare module '*.css';
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    resources: typeof resources[ 'en' ];
+    enableSelector: true;
+  }
+}
