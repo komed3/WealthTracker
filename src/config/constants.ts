df4ef26f861f3c346f2dbe5a72765d94f4460701
komed3 <pk97@outlook.de>
2026-05-18 00:05:44 +0200

    declare colors

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,6 +4,11 @@ export type CURRENCY = ( typeof CURRENCY )[ number ];
 export const ICON = [] as const;
 export type ICON = ( typeof ICON )[ number ];
 
-export const COLOR = [] as const;
+export const COLOR = [
+  '#64748b', '#ef4444', '#f97316', '#f59e0b', '#eab308',
+  '#84cc16', '#22c55e', '#10b981', '#14b8a6', '#06b6d4',
+  '#0ea5e9', '#3b82f6', '#6366f1', '#8b5cf6', '#a855f7',
+  '#d946ef', '#ec4899', '#f43f5e', '#78716c', '#71717a',
+] as const;
 export type COLOR = ( typeof COLOR )[ number ];
 
