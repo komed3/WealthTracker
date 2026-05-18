e18a346bf07a3b7f169788562df7b6dcf3b4c228
komed3 <pk97@outlook.de>
2026-05-18 11:31:02 +0200

    mount data provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
-import { MainLayout } from '@/src/components/layout/MainLayout';
+import { DataProvider } from '@/src/context/DataCtx';
 import { LayoutProvider } from '@/src/context/LayoutCtx';
+
+import { MainLayout } from '@/src/components/layout/MainLayout';
+
 import { AssetDetail } from '@/src/views/AssetDetail';
 import { Assets } from '@/src/views/Assets';
 import { Breakdown } from '@/src/views/Breakdown';
@@ -9,25 +12,29 @@ import { Dashboard } from '@/src/views/Dashboard';
 import { Editor } from '@/src/views/Editor';
 import { Momentum } from '@/src/views/Momentum';
 import { Report } from '@/src/views/Report';
+import { Settings } from '@/src/views/Settings';
 import { Stats } from '@/src/views/Stats';
 
 export default function App () {
   return (
-    <LayoutProvider>
-      <BrowserRouter>
-        <MainLayout>
-          <Routes>
-            <Route path= '/' element= { <Dashboard /> } />
-            <Route path= '/momentum' element= { <Momentum /> } />
-            <Route path= '/assets' element= { <Assets /> } />
-            <Route path= '/asset/:assetId' element= { <AssetDetail /> } />
-            <Route path= '/breakdown' element= { <Breakdown /> } />
-            <Route path= '/report' element= { <Report /> } />
-            <Route path= '/stats' element= { <Stats /> } />
-            <Route path= '/editor' element= { <Editor /> } />
-          </Routes>
-        </MainLayout>
-      </BrowserRouter>
-    </LayoutProvider>
+    <DataProvider>
+      <LayoutProvider>
+        <BrowserRouter>
+          <MainLayout>
+            <Routes>
+              <Route path= '/' element= { <Dashboard /> } />
+              <Route path= '/momentum' element= { <Momentum /> } />
+              <Route path= '/assets' element= { <Assets /> } />
+              <Route path= '/asset/:assetId' element= { <AssetDetail /> } />
+              <Route path= '/breakdown' element= { <Breakdown /> } />
+              <Route path= '/report' element= { <Report /> } />
+              <Route path= '/stats' element= { <Stats /> } />
+              <Route path= '/editor' element= { <Editor /> } />
+              <Route path= '/settings' element= { <Settings /> } />
+            </Routes>
+          </MainLayout>
+        </BrowserRouter>
+      </LayoutProvider>
+    </DataProvider>
   );
 }
