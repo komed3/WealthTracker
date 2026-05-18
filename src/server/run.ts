19c2f3b829331808ac361e0b2aff7c040129aecc
komed3 <pk97@outlook.de>
2026-05-18 11:00:51 +0200

    mv server.ts to server/run.ts

--- src/server/run.ts
+++ src/server/run.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { createServer } from 'vite';
 
+import api from '@/src/server/api';
+import { initDb } from '@/src/server/db';
+
 async function run () : Promise< void > {
   const port = process.env.PORT || 3000;
   const app = express();
+
+  initDb();
 
   const vite = await createServer( {
     server: { middlewareMode: true }
@@ -11,6 +16,7 @@ async function run () : Promise< void > {
 
   app.use( express.json() );
   app.use( express.urlencoded( { extended: true } ) );
+  app.use( '/api', api );
   app.use( vite.middlewares );
 
   app.listen( port, () => console.log( `Server startet on port ${ port }` ) );
