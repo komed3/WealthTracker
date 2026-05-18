a2da21d4587e7c8e5a92560eed7f7fcf4477f01c
komed3 <pk97@outlook.de>
2026-05-18 11:15:38 +0200

    update i18n

--- src/locales/de.ts
+++ src/locales/de.ts
@@ -1,5 +1,20 @@
 export default ( {
   translation: {
+    currencies: {
+      CHF: 'Schweizer Franken (CHF)',
+      EUR: 'Euro (EUR)',
+      USD: 'US-Dollar (USD)'
+    },
+    genders: {
+      diverse: 'Divers',
+      female: 'Weiblich',
+      male: 'Männlich',
+      unspecified: 'Keine Angabe'
+    },
+    languages: {
+      de: 'Deutsch',
+      en: 'Englisch'
+    },
     nav: {
       assets: 'Vermögenswerte',
       breakdown: 'Aufschlüsselung',
@@ -13,6 +28,19 @@ export default ( {
       collapse: 'Einklappen',
       expand: 'Aufklappen',
       settings: 'Einstellungen'
+    },
+    settings: {
+      birthDate: 'Geburtsdatum',
+      currency: 'Standardwährung',
+      decimals: 'Dezimalstellen',
+      description: 'Verwalte dein Profil und deine Anzeige-Konfigurationen.',
+      displayTitle: 'Anzeige-Einstellungen',
+      gender: 'Geschlecht',
+      language: 'Sprache',
+      profileTitle: 'Persönliches Profil',
+      save: 'Einstellungen speichern',
+      savedSuccess: 'Einstellungen erfolgreich gespeichert!',
+      title: 'Einstellungen'
     }
   }
 } ) as const;
--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,5 +1,20 @@
 export default ( {
   translation: {
+    currencies: {
+      CHF: 'Swiss Franc (CHF)',
+      EUR: 'Euro (EUR)',
+      USD: 'US Dollar (USD)'
+    },
+    genders: {
+      diverse: 'Diverse',
+      female: 'Female',
+      male: 'Male',
+      unspecified: 'Unspecified'
+    },
+    languages: {
+      de: 'German',
+      en: 'English'
+    },
     nav: {
       assets: 'Assets',
       breakdown: 'Breakdown',
@@ -13,6 +28,19 @@ export default ( {
       collapse: 'Collapse',
       expand: 'Expand',
       settings: 'Settings'
+    },
+    settings: {
+      birthDate: 'Date of Birth',
+      currency: 'Default Currency',
+      decimals: 'Decimal Places',
+      description: 'Manage your profile and display configurations.',
+      displayTitle: 'Display Settings',
+      gender: 'Gender',
+      language: 'Language',
+      profileTitle: 'Personal Profile',
+      save: 'Save Settings',
+      savedSuccess: 'Settings saved successfully!',
+      title: 'Settings'
     }
   }
 } ) as const;
