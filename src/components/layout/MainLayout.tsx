10e0a88f1e5a1d6aee8185fd0baccd6747f9398b
komed3 <pk97@outlook.de>
2026-05-18 20:33:04 +0200

    mount mobile top bar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,9 +1,12 @@
-import { Sidebar } from '@/src/components/layout/Sidebar';
 import type { LayoutProps } from '@/src/types/props';
+import { MobileTopBar } from '@/src/components/layout/MobileTopBar';
+import { Sidebar } from '@/src/components/layout/Sidebar';
 
 export const MainLayout = ( { children }: LayoutProps ) => {
+
   return (
-    <div className= 'flex h-dvh overflow-hidden bg-slate-50 text-slate-800 antialiased'>
+    <div className= 'flex flex-col lg:flex-row h-dvh overflow-hidden bg-slate-50 text-slate-800'>
+      <MobileTopBar />
       <Sidebar />
 
       <main className= 'flex-1 flex flex-col min-w-0 overflow-y-auto transition-all duration-300 ease-in-out'>
