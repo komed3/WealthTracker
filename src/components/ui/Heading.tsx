7d310a4bbb1826b38d23166d63408498ad63f756
komed3 <pk97@outlook.de>
2026-05-18 16:24:17 +0200

    update styles for heading

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -5,10 +5,10 @@ export const Heading = ( { children, level = 1, className }: HeadingProps ) => {
   const Tag = `h${ level }` as const;
 
   const styles = {
-    1: 'font-display font-bold text-3xl md:text-4xl tracking-tight text-slate-900',
-    2: 'font-display font-semibold text-2xl md:text-3xl tracking-tight text-slate-900',
-    3: 'font-display font-semibold text-xl text-slate-900',
-    4: 'font-display font-medium text-lg text-slate-800'
+    1: 'font-display font-bold text-2xl md:text-3xl tracking-tight text-slate-900',
+    2: 'font-display font-semibold text-2xl tracking-tight text-slate-800',
+    3: 'font-display font-semibold text-xl text-slate-800',
+    4: 'font-display font-medium text-lg text-slate-600'
   };
 
   return (
