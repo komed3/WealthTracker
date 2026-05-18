6830c1466100fd63f513b259fba64fc244700124
komed3 <pk97@outlook.de>
2026-05-18 11:27:34 +0200

    declare DataCtxType interface

--- src/types/context.d.ts
+++ src/types/context.d.ts
@@ -1,3 +1,13 @@
+import type { Data, Settings } from './data';
+
+export interface DataCtxType {
+  loading: boolean;
+  data: Data | null;
+  settings: Settings | null;
+  updateSettings: ( settings: Settings ) => Promise< boolean >;
+  refreshData: () => Promise< void >;
+}
+
 export interface LayoutCtxType {
   sidebarOpen: boolean;
   setSidebarOpen: ( open: boolean ) => void;
