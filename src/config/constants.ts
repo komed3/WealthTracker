c8de9e031ac95721a4cfb3d64a96f770ae0a199a
komed3 <pk97@outlook.de>
2026-05-18 09:39:04 +0200

    rework constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,36 @@
-export const CURRENCY = [ 'EUR', 'USD', 'CHF' ] as const;
 export type CURRENCY = ( typeof CURRENCY )[ number ];
-
-export const LIQUIDITY = [ 1, 2, 3, 4, 5 ] as const;
-export type LIQUIDITY = ( typeof LIQUIDITY )[ number ];
-
-export const CLASS = [
-  'cash', 'accounts', 'moneyMarket', 'stocks', 'funds',
-  'bonds', 'interest', 'pension', 'realEstate', 'commodities',
-  'tangibles', 'crypto', 'loans', 'other'
+export const CURRENCY = [
+  'EUR', 'USD', 'CHF'
 ] as const;
-export type CLASS = ( typeof CLASS )[ number ];
 
+export type LIQUIDITY = ( typeof LIQUIDITY )[ number ];
+export const LIQUIDITY = [
+  1, // immediately (< 1 week)
+  2, // short term (1 week - 6 months)
+  3, // medium term (6 months - 3 years)
+  4, // long term (3 - 10 years)
+  5, // illiquid (> 10 years)
+] as const;
+
+export type CATEGORY = ( typeof CATEGORY )[ number ];
+export const CATEGORY = [
+  'asset', 'liability'
+] as const;
+
+export type ASSET_CLASS = ( typeof ASSET_CLASS )[ number ];
+export const ASSET_CLASS = [
+  'cash', 'bank', 'moneyMarket', 'stocks', 'etf', 'funds', 'bonds',
+  'crypto', 'commodities', 'realEstate', 'pension', 'insurance',
+  'collectibles', 'vehicles', 'luxury', 'lending', 'privateEquity',
+  'other'
+] as const;
+
+export type LIABILITY_CLASS = ( typeof LIABILITY_CLASS )[ number ];
+export const LIABILITY_CLASS = [
+  'mortgage', 'loan', 'creditCard', 'tax', 'leasing', 'other'
+] as const;
+
+export type ICON = ( typeof ICON )[ number ];
 export const ICON = [
   'Wallet', 'WalletCards', 'Vault', 'PiggyBank', 'Landmark',
   'Coins', 'Bitcoin', 'Banknote', 'House', 'Car', 'Watch', 'Gem',
@@ -18,13 +38,16 @@ export const ICON = [
   'HeartHandshake', 'CreditCard', 'Building2', 'Briefcase',
   'TrendingUp', 'PieChart', 'Plane', 'Ship', 'Smartphone'
 ] as const;
-export type ICON = ( typeof ICON )[ number ];
 
+export type COLOR = ( typeof COLOR )[ number ];
 export const COLOR = [
   '#64748b', '#ef4444', '#f97316', '#f59e0b', '#eab308',
   '#84cc16', '#22c55e', '#10b981', '#14b8a6', '#06b6d4',
   '#0ea5e9', '#3b82f6', '#6366f1', '#8b5cf6', '#a855f7',
   '#d946ef', '#ec4899', '#f43f5e', '#78716c', '#71717a',
 ] as const;
-export type COLOR = ( typeof COLOR )[ number ];
 
+export type GENDER = ( typeof GENDER )[ number ];
+export const GENDER = [
+  'male', 'female', 'diverse', 'unspecified'
+] as const;
