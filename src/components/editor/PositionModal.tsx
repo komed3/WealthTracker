08e57b7f019eef7667bfd38a9e58c846786dea14
komed3 <pk97@outlook.de>
2026-05-19 23:58:01 +0200

    min fix

--- src/components/editor/PositionModal.tsx
+++ src/components/editor/PositionModal.tsx
@@ -188,7 +188,7 @@ export const PositionModal = ( { isOpen, onClose, onSave, initialEntry }: Positi
                     <div className= {
                       'w-11 h-6 bg-slate-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full ' +
                       'peer-checked:after:border-white after:content-[""] after:absolute after:top-0.5 after:left-0.5 ' +
-                      'after:bg-white after:border-slate-350 after:border after:rounded-full after:h-5 after:w-5 after:transition-all ' +
+                      'after:bg-white after:border-slate-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all ' +
                       'peer-checked:bg-primary'
                     } />
                   </label>
