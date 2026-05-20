21b39e6054c316f1efa5ed59d455fb055a4377f4
komed3 <pk97@outlook.de>
2026-05-20 03:46:34 +0200

    update colors

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -66,9 +66,10 @@ export const ICON = [
 export type COLOR = ( typeof COLOR )[ number ];
 export const COLOR = [
   '#64748b', '#ef4444', '#f97316', '#f59e0b', '#eab308',
+  '#d4a373', '#bc6c25', '#606c38', '#a6912b', '#a3b18a',
   '#84cc16', '#22c55e', '#10b981', '#14b8a6', '#06b6d4',
   '#0ea5e9', '#3b82f6', '#6366f1', '#8b5cf6', '#a855f7',
-  '#d946ef', '#ec4899', '#f43f5e'
+  '#d946ef', '#e0bbe4', '#ec4899', '#f43f5e', '#a53f2b'
 ] as const;
 
 export type GENDER = ( typeof GENDER )[ number ];
