b19256f4e59d9df00ade00da5a8c434908619078
komed3 <pk97@outlook.de>
2026-05-20 03:02:24 +0200

    fix uuid method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,16 +2,7 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function uuid () {
-  if ( typeof crypto !== 'undefined' && crypto.randomUUID ) return crypto.randomUUID();
-
-  const bytes = new Uint8Array( 16 );
-  ( typeof crypto !== 'undefined' ? crypto.getRandomValues : require( 'crypto' ).randomFillSync )( bytes );
-
-  bytes[ 6 ] = ( bytes[ 6 ] & 0x0f ) | 0x40;
-  bytes[ 8 ] = ( bytes[ 8 ] & 0x3f ) | 0x80;
-
-  const hex = [ ...bytes ].map( b => b.toString( 16 ).padStart( 2, '0' ) ).join( '' );
-  return hex.substring( 0, 16 );
+  return window.crypto.randomUUID();
 }
 
 export function cn ( ...inputs: ClassValue[] ) {
