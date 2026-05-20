2a2d5f007180c1df53c4435231aa9b1b59d0af62
komed3 <pk97@outlook.de>
2026-05-20 03:09:08 +0200

    chunking vite build assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,21 @@ import { defineConfig } from 'vite';
 
 export default defineConfig ( {
   plugins: [ react(), tailwindcss() ],
-  resolve: { alias: { '@': resolve( __dirname, '.' ) } }
+  resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks ( id ) {
+          if ( id.includes( 'node_modules' ) ) {
+            if ( id.includes( 'lucide' ) ) return 'icons';
+            if ( id.includes( 'recharts' ) ) return 'charts';
+            if ( id.includes( 'motion' ) ) return 'motion';
+            if ( id.includes( 'react' ) ) return 'react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 } );
