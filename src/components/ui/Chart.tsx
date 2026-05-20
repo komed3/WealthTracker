75c84e9dcb15d7ee1776a544a00f878ba89dfbd7
komed3 <pk97@outlook.de>
2026-05-20 23:06:38 +0200

    implement custom tooltip

--- src/components/ui/Chart.tsx
+++ src/components/ui/Chart.tsx
@@ -2,10 +2,18 @@ import { useData } from '@/src/context/DataCtx';
 import { formatCurrency, formatPercent } from '@/src/lib/formatter';
 import type { CustomTooltipProps, yAxisFormatterProps } from '@/src/types/props';
 
-export const CustomTooltip = ( { label }: CustomTooltipProps ) => {
+export const CustomTooltip = ( { label, value, children, color }: CustomTooltipProps ) => {
   return (
     <div className= 'min-w-50 p-4 bg-white border border-slate-200 rounded-xl shadow-md'>
-      <p className= 'mb-1 uppercase font-bold text-xs text-slate-400 tracking-widest'>{ label }</p>
+      <p className= 'uppercase font-bold text-sm text-slate-400 tracking-wider'>{ label }</p>
+      <div className= 'text-lg font-bold' style= { { color } }>
+        { value }
+      </div>
+      { children && (
+        <div className= 'space-y-1.5 mt-2 pt-2 text-xs text-slate-500 border-t border-dashed border-slate-300'>
+          { children }
+        </div>
+      ) }
     </div>
   );
 };
