ed5119001d88dd93deaa2252bab1fe304e03cbea
komed3 <pk97@outlook.de>
2026-05-21 10:52:09 +0200

    scroll top on location change

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,16 +3,25 @@ import { Sidebar } from '@/src/components/layout/Sidebar';
 import { Loading } from '@/src/components/ui/Loading';
 import { useData } from '@/src/context/DataCtx';
 import type { LayoutProps } from '@/src/types/props';
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router';
 
 export const MainLayout = ( { children }: LayoutProps ) => {
   const { loading } = useData();
+  const location = useLocation();
+  const mainRef = useRef < HTMLDivElement > ( null );
+
+  useEffect( () => { if ( mainRef.current ) mainRef.current.scrollTop = 0 }, [ location.pathname ] );
 
   return loading ? <Loading className= 'bg-slate-50' /> : (
     <div className= 'flex flex-col lg:flex-row h-dvh overflow-hidden bg-slate-50 text-slate-800'>
       <MobileTopBar />
       <Sidebar />
 
-      <main className= 'flex-1 flex flex-col min-w-0 overflow-y-auto transition-all duration-300 ease-in-out'>
+      <main
+        className= 'flex-1 flex flex-col min-w-0 overflow-y-auto transition-all duration-300 ease-in-out'
+        ref= { mainRef }
+      >
         <div className= 'flex-1 flex flex-col w-full max-w-400 mx-auto px-8 py-8 md:px-12 md:py-10'>
           { children }
         </div>
