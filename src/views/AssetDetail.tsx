28130895e196960d5eab98b9dc97c61cd61b7e3e
komed3 <pk97@outlook.de>
2026-05-21 10:58:47 +0200

    prep. asset detail page

--- src/views/AssetDetail.tsx
+++ src/views/AssetDetail.tsx
@@ -1,3 +1,21 @@
+import { useData } from '@/src/context/DataCtx';
+import { useLayout } from '@/src/context/LayoutCtx';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router';
+
 export const AssetDetail = () => {
+  const { assetId } = useParams < { assetId: string } > ();
+  const navigate = useNavigate();
+  const { setTitle } = useLayout();
+
+  const { data, settings } = useData();
+  const assetData = data?.entries.filter( r => r.entry.id === assetId )[ 0 ];
+  const display = settings!.display;
+
+  useEffect( () => { if ( ! assetId || ! assetData ) navigate( '/assets', { replace: true } ) }, [ assetId, navigate ] );
+  if ( ! assetId || ! assetData ) return null;
+
+  useEffect( () => { setTitle( assetData.entry.title ) }, [ setTitle, display.language ] );
+
   return ( <></> );
 };
