d85870f2238aa404add5f5f154e08656db13fd16
komed3 <pk97@outlook.de>
2026-05-23 11:36:07 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,9 +12,6 @@ export function cn ( ...inputs: ClassValue[] ) {
 
 export function hasRenderableChildren ( children: ReactNode ) : boolean {
   if ( children === null || children === undefined || children === false || children === true ) return false;
-  if ( Array.isArray( children ) ) return children.some( ( child ) =>
-      child !== null && child !== undefined && child !== false && child !== true
-  );
-
+  if ( Array.isArray( children ) ) return children.some( ( child ) => hasRenderableChildren( child ) );
   return typeof children === 'string' || typeof children === 'number' || React.isValidElement( children );
 }
