1ae1b8d4d7bee0d2647c6cd916854336d8076b85
komed3 <pk97@outlook.de>
2026-05-23 14:52:16 +0200

    split dev/prod server distribution

--- src/server/run.ts
+++ src/server/run.ts
@@ -1,7 +1,10 @@
 import api from '@/src/server/api';
 import db from '@/src/server/db';
 import express from 'express';
-import { createServer } from 'vite';
+import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
 
 async function run () : Promise< void > {
   const port = process.env.PORT || 3000;
@@ -9,14 +12,21 @@ async function run () : Promise< void > {
 
   db.initDb();
 
-  const vite = await createServer( {
-    server: { middlewareMode: true }
-  } );
-
   app.use( express.json() );
   app.use( express.urlencoded( { extended: true } ) );
   app.use( '/api', api );
-  app.use( vite.middlewares );
+
+  if ( process.env.NODE_ENV === 'dev' ) {
+    const { createServer } = await import( 'vite' );
+    const vite = await createServer( { server: { middlewareMode: true } } );
+
+    app.use( vite.middlewares );
+  } else {
+    const clientDist = resolve( __dirname, '../../client' );
+
+    app.use( express.static( clientDist ) );
+    app.get( '/{*splat}', ( _, res ) => { res.sendFile( join( clientDist, 'index.html' ) ) } );
+  }
 
   app.listen( port, () => console.log( `Server startet on port ${ port }` ) );
 }
